1db795cc9d724de8e5494a5ded06360a8245574f
lucaspires31 <lucas.alvespires31@gmail.com>
2026-04-13 22:41:21 -0300

    feat: structure initial back-end

---
+++ lumiereclinic/src/main/java/com/lumiereclinic/model/Cliente.java
@@ -0,0 +1,25 @@
+package com.lumiereclinic.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Setter
+public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+    private String cpf;
+    private String telefone;
+    private String email;
+
+}
