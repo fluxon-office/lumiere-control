4e8e5ad041a239f5fb2198993c40795d8bbe0a89
lucaspires31 <lucas.alvespires31@gmail.com>
2026-04-15 20:19:27 -0300

    git add . git commit -m "feat: initial backend structure with user entity, repository, service and controller"

---
+++ lumiereclinic_backend/src/main/java/com/lumiereclinic/LumiereclinicApplication.java
@@ -0,0 +1,13 @@
+package com.lumiereclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LumiereclinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LumiereclinicApplication.class, args);
+    }
+
+}
---
+++ lumiereclinic_backend/src/test/java/com/lumiereclinic/LumiereclinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lumiereclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LumiereclinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
