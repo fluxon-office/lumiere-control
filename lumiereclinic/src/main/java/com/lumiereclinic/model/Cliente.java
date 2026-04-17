56b6db088d5da3ea17080e82bf15df54427f97ec
lucaspires31 <lucas.alvespires31@gmail.com>
2026-04-16 21:15:25 -0300

    feat: create Cliente entity for public appointment requests

---
+++ lumiereclinic/src/main/java/com/lumiereclinic/model/Cliente.java
@@ -0,0 +1,38 @@
+package com.lumiereclinic.model;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "clientes")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String telefone;
+
+    @Column(nullable = false)
+    private LocalDateTime criadoEm;
+
+    @PrePersist
+    public void prePersist() {
+        this.criadoEm = LocalDateTime.now();
+    }
+}
